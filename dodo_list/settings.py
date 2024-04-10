0fe25931140d80846a1d8c26bc4d27cab58eef71
Kyrylo <kbuchinskiy1@gmail.com>
2024-04-10 11:40:27 +0300

    fixes and new host

--- dodo_list/settings.py
+++ dodo_list/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "https://todo-tist.onrender.com"]
 
 
 # Application definition
